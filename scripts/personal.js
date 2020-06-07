bf7c6ca38d00c8869261825e7e7949bf606a909e
Niraj shrestha <CrestNiraj12>
2020-06-08 01:28:29 +0545

    Added animations

--- scripts/personal.js
+++ scripts/personal.js
@@ -14,8 +14,24 @@ $(window).on("load", function() {
     }, 20);
     
     $("#nav2-3").addClass("active-nav");
+    
+    $(".desc-head-main").css({transform: "translateY(0)", opacity: "1", height: "65vh"});
+    $(".desc-head-left").css({transform: "translateX(0)", opacity: "1"});                
 })
 
 $("nav2 > ul > li > a").on("click", function() {
     $("#nav2-3").removeClass("active-nav");
 });
+
+var texts = ["Programming", "Travelling", "FPS Games", "Animals", "Food", "Testing new features", "Learning new things", "Challenges"];
+var i = 0;
+
+function changeText() {
+    $("#hobbies").text(texts[i]);
+    if (i == 7) i = 0;
+    if (++i < texts.length){
+        setTimeout(changeText, 2000);
+    }
+}
+
+changeText();
