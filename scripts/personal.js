9a410ce7d1fc096c4eea9f10f889823723361028
Niraj shrestha <CrestNiraj12>
2020-06-08 14:19:49 +0545

    Added animations

--- scripts/personal.js
+++ scripts/personal.js
@@ -3,8 +3,8 @@ $(window).on("load", function() {
     var xp = 0, yp = 0;
 
     $(document).mousemove(function(e){
-        mouseX = e.pageX - 10;
-        mouseY = e.pageY - 10; 
+        mouseX = e.pageX - 25;
+        mouseY = e.pageY - 25; 
     });
 
     setInterval(function(){
@@ -16,21 +16,45 @@ $(window).on("load", function() {
     $("#nav2-3").addClass("active-nav");
     
     $(".desc-head-main").css({transform: "translateY(0)", opacity: "1", height: "65vh"});
-    $(".desc-head-left").css({transform: "translateX(0)", opacity: "1"});                
+    $(".desc-head-left").css({transform: "translateX(0)", opacity: "1"});
 })
+
+$("li > a").on({
+    mouseenter: function() {
+        $(".mover").css({width: $(this).width()+10, height: $(this).height()+10});
+    },
+    mouseleave: () => {
+        $(".mover").css({width: "10px", height: "10px"});
+    }
+});
+
+var preScroll = 0;
+$(window).scroll(() => {
+    var topScroll = $(window).scrollTop();
+    
+    if (topScroll > $("#content").offset().top-300) {
+        $(".mover").css({borderColor: "#000"});
+    } else if (topScroll < $("#content").offset().top) {
+        $(".mover").css({borderColor: "#fff"});
+    }
+});
+
 
 $("nav2 > ul > li > a").on("click", function() {
     $("#nav2-3").removeClass("active-nav");
 });
 
-var texts = ["Programming", "Travelling", "FPS Games", "Animals", "Food", "Testing new features", "Learning new things", "Challenges"];
+var texts = ["Programming", "Travelling", "FPS Games", "Animals", "Food", "Testing new features", "Learning new things", "Challenges", "Binge Anime", "Marvel"];
 var i = 0;
 
 function changeText() {
-    $("#hobbies").text(texts[i]);
+    $("#hobbies").css("width", "0");
+    setTimeout(() => {
+        $("#hobbies").text(texts[i]).css("width", "40%");
+    }, 2000);
     if (i == 7) i = 0;
     if (++i < texts.length){
-        setTimeout(changeText, 2000);
+        setTimeout(changeText, 4000);
     }
 }
 
