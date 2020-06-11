ec67c6fc868441062acc57b3f566d5a5f7f1a3ee
Niraj shrestha <CrestNiraj12>
2020-06-11 16:05:16 +0545

    Stable release

--- scripts/personal.js
+++ scripts/personal.js
@@ -14,86 +14,147 @@ $(window).on("load", function() {
     }, 20);
     
     $("#nav2-3").addClass("active-nav");
-    
-    $(".desc-head-main").css({transform: "translateY(0)", opacity: "1", height: "65vh"});
+    if ($(window).width() < 450) $(".desc-head-main").css({transform: "translateY(0)", opacity: "1", height: "25vh"});
+    else if ($(window).width() < 800) $(".desc-head-main").css({transform: "translateY(0)", opacity: "1", height: "45vh"});
+    else if ($(window).width() < 1000) $(".desc-head-main").css({transform: "translateY(0)", opacity: "1", height: "60vh"});
+    else $(".desc-head-main").css({transform: "translateY(0)", opacity: "1", height: "65vh"});
     $(".desc-head-left").css({transform: "translateX(0)", opacity: "1"});
+    
 })
 
-$("li > a").on({
-    mouseenter: function() {
-        $(".mover").css({width: $(this).width()+10, height: $(this).height()+10});
-    },
-    mouseleave: () => $(".mover").css({width: "10px", height: "10px"})
-});
+
+if ($(window).width() > 1000) {
+    
+    $("li > a, .projects > p > a").on({
+        mouseenter: function() {
+            $(".mover").css({width: $(this).width()+10, height: $(this).height()+10});
+        },
+        mouseleave: () => $(".mover").css({width: "10px", height: "10px"})
+    });
+    
+    function hoverText(text) {
+        $(".mover").css({width: "100px", height: "100px"}).append(`<p class='mover-text'>${text}</p>`);
+    }
+
+    function resetMover() {
+        $(".mover").css({width: "10px", height: "10px", borderRadius: "50%"}).text("")
+    }
+
+    $(".skill h1").on({
+        mouseenter: function (){ $(".mover").css({width: $(this).width(), height: $(this).height(), borderRadius: "0"}) }, 
+        mouseleave: () => resetMover()
+    })
+
+    $(".main-1, .main-2, .web-1, .web-2, .web-3, .frame-1, .back, .db").on({
+        mouseenter: function () {hoverText($(this).children()[1].textContent)},
+        mouseleave: () => resetMover()
+    })
+    
+    $(".header > .stickNav, .header > #overlay, .header > .nav2").css("display", "none");
+} else {
+    $("body > .stickNav, body > #overlay, body > .nav2").css("display", "none");
+}
+
 
 var preScroll = 0;
 var move = 0;
 $(window).scroll(() => {
     var topScroll = $(window).scrollTop();
-    
-    if (topScroll > $("#content").offset().top-300) {
-        $(".mover").css({borderColor: "#000"});
-        if (topScroll > preScroll) {
-            $(".stickNav").css({height: "0", transform: "scale(1)", opacity: "0", zIndex: "-1"}); 
-        } else{
-            $(".stickNav").css({height: "90px", transform: "scale(1)", opacity: "1", zIndex: "999"}); 
+    if ($(window).width() > 1000)  {
+        if (topScroll > $("#content").offset().top-300) {
+            $(".mover").css({borderColor: "#000"});
+            if (topScroll > preScroll) {
+                $(".stickNav").css({height: "0", transform: "scale(1)", opacity: "0", zIndex: "-1"}); 
+            } else{
+                $(".stickNav").css({height: "90px", transform: "scale(1)", opacity: "1", zIndex: "999"}); 
+            }
+        } else if (topScroll < $("#content").offset().top) {
+            $(".mover").css({borderColor: "#fff"});
         }
-    } else if (topScroll < $("#content").offset().top) {
-        $(".mover").css({borderColor: "#fff"});
     }
     
     $(".skill").each(function (n,e) {
         if (topScroll >= $(e).offset().top-400 && topScroll < $(e).offset().top+350) {
             $(e.children[0].children[0]).css("filter", "saturate(0.5) brightness(1)");
-            $(e.children[1]).css({transform: "translateX(0)", filter: "blur(0)", opacity: "1"});
+            if ($(window).width() > 800) {
+                $(e.children[1]).css({transform: "translateX(0)", filter: "blur(0)", opacity: "1"});
+                if ($(window).width() <= 1000 && e.classList.contains("skill-right")) $(e.children[1]).css({transform: "translateX(18vw)", filter: "blur(0)", opacity: "1"});
+            } else {
+                $(e.children[1]).css({transform: "translateX(10vh)", filter: "blur(0)", opacity: "1"});
+                if (e.classList.contains("skill-right")) $(e.children[1]).css({transform: "translateX(15vw)", filter: "blur(0)", opacity: "1"});
+            }
         } else {
             $(e.children[0].children[0]).css("filter", "saturate(0) brightness(2)");
-            if (e.classList.contains("skill-left")) $(e.children[1]).css({transform: "translateX(15vw)", filter: "blur(0)", opacity: "1"});
-            else $(e.children[1]).css({transform: "translateX(-15vw)", filter: "blur(0)", opacity: "1"});
+            if ($(window).width() > 800) {
+                if (e.classList.contains("skill-left")) 
+                    $(e.children[1]).css({transform: "translateX(15vw)", filter: "blur(0)", opacity: "1"});
+                else {
+                    if ($(window).width() > 1000) $(e.children[1]).css({transform: "translateX(-15vw)", filter: "blur(0)", opacity: "1"});
+                    else $(e.children[1]).css({transform: "translateX(0px)", filter: "blur(0)", opacity: "1"});
+                }
+            }
+            else {
+                if (e.classList.contains("skill-left")) 
+                    $(e.children[1]).css({transform: "translateX(25vw)", filter: "blur(0)", opacity: "1"});
+                else {
+                    $(e.children[1]).css({transform: "translateX(0)", filter: "blur(0)", opacity: "1"});
+                }
+            }
         }
     });
     
-  
-    $("#left").css("transform", "translateX("+ (topScroll - $("#left").offset().top + 150) + "px)");
-    $("#right").css("transform", "translateX("+ (-topScroll + $("#right").offset().top - 300) + "px)");
+    $(".card").each(function(n,e) {
+        if (topScroll >= $(e).offset().top-700) {
+            $(e).css({transform: "translateY(0)", opacity: "1"});
+        } else {
+            $(e).css({transform: "translateY(100px)", opacity: "0"});
+        }
+    });
     
+    if ($(window).width() > 450) {
+        $("#left").css("transform", "translateX("+ (topScroll - $("#left").offset().top + 150) + "px)");
+        $("#right").css("transform", "translateX("+ (-topScroll + $("#right").offset().top - 300) + "px)");
+    } else {
+        $("#left").css("transform", "translateX("+ (topScroll - $("#left").offset().top + 200) + "px)");
+        $("#right").css("transform", "translateX("+ (-topScroll + $("#right").offset().top - 250) + "px)");
+    }
     preScroll = topScroll;
 });
 
-function hoverText(text) {
-    $(".mover").css({width: "100px", height: "100px"}).append(`<p class='mover-text'>${text}</p>`);
-}
 
-function resetMover() {
-    $(".mover").css({width: "10px", height: "10px", borderRadius: "50%"}).text("")
-}
-
-$(".skill h1").on({
-    mouseenter: function (){ $(".mover").css({width: $(this).width(), height: $(this).height(), borderRadius: "0"}) }, 
-    mouseleave: () => resetMover()
-})
-
-$(".main-1, .main-2, .web-1, .web-2, .web-3, .frame-1, .back, .db").on({
-    mouseenter: function () {hoverText($(this).children()[1].textContent)},
-    mouseleave: () => resetMover()
-})
 
 $("nav2 > ul > li > a").on("click", function() {
     $("#nav2-3").removeClass("active-nav");
 });
 
 var texts = ["Programming", "Travelling", "FPS Games", "Animals", "Food", "Testing new features", "Learning new things", "Challenges", "Binge Anime", "Marvel"];
-var i = 0;
+var designs = ["design", "experience", "branding", "identity", "website"];
+var i = 0, j = 0;
 
 function changeText() {
     $("#hobbies").css("width", "0");
     setTimeout(() => {
         $("#hobbies").text(texts[i]).css("width", "40%");
+        i++;
     }, 2000);
-    if (i == 7) i = 0;
-    if (++i < texts.length){
+    if (i == 10) i = 0;
+    if (i < texts.length){
         setTimeout(changeText, 4000);
     }
 }
 
+function changeCollabText() {
+    $("#design").css("width", "0");
+    setTimeout(() => {
+        if ($(window).width() > 450) $("#design").text(designs[j]).css("width", "20%");
+        else $("#design").text(designs[j]).css("width", "100%");
+        j++;
+    }, 2000);
+    if (j == 5) j = 0;
+    if (j < designs.length){
+        setTimeout(changeCollabText, 4000);
+    }
+}
+
 changeText();
+changeCollabText();
